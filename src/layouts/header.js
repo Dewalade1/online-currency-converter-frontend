0403c38c7f0851634d1d306fa5f261067147a4f0
Dewa Ade-onojobi <adewalade@gmail.com>
2021-08-23 21:45:24 +0100

    feat: add `main title` style

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -5,7 +5,10 @@ const Header = () => {
     <header className="align-items-center">
       <div className="align-items-center">
         <img src="./assets/images/logo.png" alt="" className="app-logo" />
-        <span id="app-title">Currency Converter</span>
+        <span id="app-title">
+          <div className="text--main-title-top">Currency</div>
+          <div className="text--light-blue text--main-title-bottom">Converter</div>
+        </span>
       </div>
     </header>
   );
